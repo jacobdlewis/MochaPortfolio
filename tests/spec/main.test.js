89006cbd3dd857a04cba7b4709003f344334f00c
jacobdlewis <jacobdlewis@gmail.com>
2015-02-05 12:30:05 -0600

    added additional tests to check appending elements created with ajax response data

--- tests/spec/main.test.js
+++ tests/spec/main.test.js
@@ -22,17 +22,26 @@ describe('getStock', function () {
    });
     it('should return another stock object', function (done){
     getStock('MSFT', function(stock){
-      stock.Name.should.equal('Microsrp Corp');
+      stock.Name.should.equal('Microsoft Corp');
       done();
     });
   });
 });
   describe('addStockToTable', function() {
-    it('should use stock info to append the table', function() {
+    it('should add a row to the table', function() {
       var stock = { Name: "SuperCorp", Symbol: "SCRP", LastPrice: 34.32 };
       $('tr').length.should.equal(0);
       addStockToTable(stock);
       $('tr').length.should.equal(1);
+    });
+    it('should add two stocks to the table, using stock data from ajax call', function() {
+      var stock = { Name: "SuperCorp", Symbol: "SCRP", LastPrice: 34.32 };
+           $row = addStockToTable(stock);
+           $tds = $row.find('td');
+      $tds.length.should.equal(3);
+      $($tds[0]).text().should.equal('SuperCorp');
+      $($tds[1]).text().should.equal('SCRP');
+      $($tds[2]).text().should.equal('34.32');
     });
   });
 
