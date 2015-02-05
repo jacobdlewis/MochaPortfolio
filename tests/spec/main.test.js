64d1b8a0431d731d483f77540354d08495c37467
jacobdlewis <jacobdlewis@gmail.com>
2015-02-05 12:10:55 -0600

    added several tests for async function

--- tests/spec/main.test.js
+++ tests/spec/main.test.js
@@ -1,8 +1,39 @@
 /* jshint mocha: true, expr: true, strict: false */
 
-describe('test suite', function () {
+describe('test suite', function() {
   it('should assert true', function () {
     true.should.be.true;
     false.should.be.false;
   });
 });
+
+describe('hello', function () {
+  it('should return world', function () {
+    hello().should.equal('world');
+  });
+});
+
+describe('getStock', function () {
+  it('should return the stock object', function (done){
+    getStock('AAPL', function(stock){
+      stock.Name.should.equal('Apple Inc');
+      done();
+    });
+   });
+    it('should return another stock object', function (done){
+    getStock('MSFT', function(stock){
+      stock.Name.should.equal('Microsrp Corp');
+      done();
+    });
+  });
+});
+  describe('addStockToTable', function() {
+    it('should use stock info to append the table', function() {
+      var stock = { Name: "SuperCorp", Symbol: "SCRP", LastPrice: 34.32 };
+      $('tr').length.should.equal(0);
+      addStockToTable(stock);
+      $('tr').length.should.equal(1);
+    });
+  });
+
+
