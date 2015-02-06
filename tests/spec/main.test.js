e1c90ae71f01eb4b91aae6c7dbf4d6f8c7eea1af
jacobdlewis <jacobdlewis@gmail.com>
2015-02-06 09:22:30 -0600

    added tests for refresh function

--- tests/spec/main.test.js
+++ tests/spec/main.test.js
@@ -38,11 +38,41 @@ describe('getStock', function () {
       var stock = { Name: "SuperCorp", Symbol: "SCRP", LastPrice: 34.32 };
            $row = addStockToTable(stock);
            $tds = $row.find('td');
-      $tds.length.should.equal(3);
+      $tds.length.should.equal(4);
       $($tds[0]).text().should.equal('SuperCorp');
       $($tds[1]).text().should.equal('SCRP');
       $($tds[2]).text().should.equal('34.32');
+      $($tds[3]).text().should.equal('34.32');
     });
   });
 
+  describe('getMultipleStocks', function () {
+    it('should return multiple stock objects', function (done){
+      getMultipleStocks(['AAPL', 'MSFT'], function(stocks){
+        stocks.length.should.equal(2);
+        stocks[0].Name.should.equal('Apple Inc');
+        stocks[1].Name.should.equal('Microsoft Corp');
+        done();
+      });
+    });
+  });
+
+describe('refreshStockPrices', function () {
+  it('should edit each stock in the table with a new price', function () {
+    var stocks = [
+      {Symbol: 'AAPL', LastPrice: 12.45},
+      {Symbol: 'MSFT', LastPrice: 23.56}
+    ];
+    $tbody = $('tbody'),
+    $trs   = $('tr');
+
+    $('tbody').append('<tr><td>Apple Inc</td><td>AAPL</td><td>12.34</td><td>12.34</td></tr>');
+    $('tbody').append('<tr><td>Microsoft Corp</td><td>MSFT</td><td>23.34</td><td>23.45</td></tr>');
+
+    refreshStockPrices(stocks);
+
+    $($($trs[0]).find('td')[3]).text().should.equal('12.45');
+    $($($trs[1]).find('td')[3]).text().should.equal('23.56');
+  });
+});
 
